113730f1d43bda09d325f574cccc78770599eef3
Bertmoss <b3rtm0ss@gmail.com>
2021-12-12 17:15:02 +0100

    updated README.md ; Added images; Added UI and modified index.html script.js style.css

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # rock_paper_scissors_game
+Title: Rock Paper Scissors Game
+Description: A rock, paper, scissors game. A brief description of the rules can be found here: https://www.wikihow.com/Play-Rock,-Paper,-Scissors
+Technologies used: Javascript, HTML, CSS
+Created for the Odin Project's Foundation Course Requirement: https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors
+
+Color Pattern Used:
+vivid-tangerine: #F1AB91ff;
+cadmium-orange: #FA8928ff;
+melon: #FFBEA8ff;
+alice-blue: #F2F7FAff;
+gainsboro: #DBE0E3ff;
+
--- script.js
+++ script.js
@@ -1,86 +1,190 @@
-//Gets user Input. If the input is incorrect requests it again.
-function getUserInput() {
-  let userInput = prompt("Rock, Paper, or Scissors?", "");
-  checkUserInput(userInput);
-  while (checkUserInput(userInput) === false) {
-    alert("Incorrect Input!");
-    userInput = prompt("Rock, Paper, or Scissors?", "");
-    checkUserInput(userInput);
-  }
-  return userInput;
-}
-//Checks if user input is correct
-function checkUserInput(userChoice) {
-  userChoice = userChoice.toUpperCase();
-  if (
-    userChoice === "ROCK" ||
-    userChoice === "PAPER" ||
-    userChoice === "SCISSORS"
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-}
+//Arrays
+//Score of the games played
+const tiedGames = [];
+const scoreUser = [];
+const scoreComputer = [];
+const gamesPlayed = [];
+const currentGameArr = [];
 
-//create random computer input
+//Computer quips
+const playerLoseQuips = [
+  "Sweet Victory!",
+  "That was too easy!",
+  "Winner, winner, chicken dinner!",
+  "Have you gotten used to losing yet?",
+  "Ha, I win!",
+];
+const playerWinQuips = [
+  "Tis but a scratch.",
+  "You got lucky!",
+  "Welp, that's unexpected...",
+  "I'll get you next time!",
+  "I'm still far superior to you human!",
+];
+const playerTieQuips = [
+  "I guess we're both losers.",
+  "I guess we're both winners.",
+  "I'm tie-red of ties.",
+  "Great minds think alike!",
+  "Stop copying me!",
+];
+
+//Functions
+//Create random computer input
 function getComputerInput() {
   const options = ["Rock", "Paper", "Scissors"];
   let randomNum = Math.floor(Math.random() * options.length);
   return options[randomNum];
 }
-//Arrays to keep score of the games played
-const tiedGames = [];
-const scoreUser = [];
-const scoreComputer = [];
-
-// compare user variable to computer variable, inform user of the result, add result to the score arrays.
+//Compare user variable to computer variable, inform user of the result, add result to the score arrays.
 function determineWinner(user, computer) {
   user = user.toUpperCase();
   computer = computer.toUpperCase();
   if (user === computer) {
+    currentGameArr.push("It's a tie!");
+    gamesPlayed.push(`${user.toLowerCase()} = ${computer.toLowerCase()}`);
     tiedGames.push(1);
-    return alert("It's a tie!");
+    return "tie";
   } else if (
     (user === "ROCK" && computer === "SCISSORS") ||
     (user === "SCISSORS" && computer === "PAPER") ||
     (user === "PAPER" && computer === "ROCK")
   ) {
+    currentGameArr.push("You win!");
+    gamesPlayed.push(`${user.toLowerCase()} > ${computer.toLowerCase()}`);
     scoreUser.push(1);
-    return alert(
-      `You win, ${user.toLowerCase()} beats ${computer.toLowerCase()}!`
-    );
+    return "win";
   } else {
+    currentGameArr.push("You lose!");
+    gamesPlayed.push(`${user.toLowerCase()} < ${computer.toLowerCase()}`);
     scoreComputer.push(1);
-    return alert(
-      `You lose, ${computer.toLowerCase()} beats ${user.toLowerCase()}!`
-    );
+    return "loss";
   }
 }
-//Let's play alert
-alert("Let's play Rock, Paper, Scissors!");
-//Play the game 5 times
-for (let i = 1; i <= 5; i++) {
-  alert(`Round: ${i}`);
-  determineWinner(getUserInput(), getComputerInput());
+//Create computer quip
+function getComputerQuip(arr) {
+  let randomNum = Math.floor(Math.random() * arr.length);
+  return arr[randomNum];
 }
-//Determine winner of the 5 games. Inform the user of the scores.
-function determineOverallWinner(scoreOne, scoreTwo, scoreThree) {
-  if (scoreOne.length > scoreTwo.length) {
-    return alert(`You are a winner! 
-    You: ${scoreOne.length} 
-    Computer: ${scoreTwo.length} 
-    Tied Games: ${scoreThree.length}`);
-  } else if (scoreOne.length < scoreTwo.length) {
-    return alert(`You are a loser! 
-    You: ${scoreOne.length} 
-    Computer: ${scoreTwo.length} 
-    Tied Games: ${scoreThree.length}`);
-  } else {
-    return alert(`It's a tie! 
-    You: ${scoreOne.length} 
-    Computer: ${scoreTwo.length} 
-    Tied Games: ${scoreThree.length}`);
+//Removes classes
+function removeClass(elem, cls) {
+  if (elem.classList.contains(cls)) {
+    return elem.classList.remove(cls);
   }
 }
-determineOverallWinner(scoreUser, scoreComputer, tiedGames);
+//If not the first click reset animation
+function resetAnimation(elem) {
+  removeClass(elem, "victory");
+  removeClass(elem, "defeat");
+  return elem;
+}
+
+//DOM Manipulation
+//Selectors
+//Score
+const wins = document.querySelector("#wins");
+const losses = document.querySelector("#losses");
+const ties = document.querySelector("#ties");
+const rounds = document.querySelector("#rounds");
+//Notifications
+const currentGame = document.querySelector("#currentGame");
+const previousGame = document.querySelector("#previousGame");
+const previousRound = document.querySelector("#previousRound");
+const previousGameTwo = document.querySelector("#previousGameTwo");
+const previousRoundTwo = document.querySelector("#previousRoundTwo");
+const theComputer = document.querySelector("#theComputer");
+const quips = document.querySelector("#quips");
+//Images
+const playerImage = document.querySelector("#playerImage");
+const computerImage = document.querySelector("#computerImage");
+//Buttons
+const buttons = document.querySelectorAll("button");
+//Event listener
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    //Computer input value
+    let pcInput = getComputerInput();
+    //Background images for the player
+    if (button.id === "paper") {
+      playerImage.setAttribute(
+        "style",
+        "background-image: url(/images/paper.PNG)"
+      );
+    } else if (button.id === "rock") {
+      playerImage.setAttribute(
+        "style",
+        "background-image: url(/images/rock.PNG)"
+      );
+    } else {
+      playerImage.setAttribute(
+        "style",
+        "background-image: url(/images/scissors.PNG)"
+      );
+    }
+    //Background images for the computer input
+    if (pcInput === "Paper") {
+      computerImage.setAttribute(
+        "style",
+        "background-image: url(/images/paper.PNG)"
+      );
+    } else if (pcInput === "Rock") {
+      computerImage.style.cssText = "background-image: url(/images/rock.PNG)";
+    } else {
+      computerImage.style.cssText =
+        "background-image: url(/images/scissors.PNG)";
+    }
+    //If not the first game removes previous classes for the border animation
+    resetAnimation(playerImage);
+    resetAnimation(computerImage);
+
+    let result = determineWinner(button.id, pcInput);
+    currentGame.textContent = `${currentGameArr[currentGameArr.length - 1]}`;
+    previousRound.textContent = `Round ${gamesPlayed.length - 1}`;
+    previousGame.textContent = `${gamesPlayed[gamesPlayed.length - 2]}`;
+    previousRoundTwo.textContent = `Round ${gamesPlayed.length - 2}`;
+    previousGameTwo.textContent = `${gamesPlayed[gamesPlayed.length - 3]}`;
+    removeClass(theComputer, "hide");
+
+    if (result === "win") {
+      rounds.textContent = `Round: ${gamesPlayed.length}`; //update number of rounds
+      wins.textContent = `Wins: ${scoreUser.length}`; // update wins
+      quips.textContent = `${getComputerQuip(playerWinQuips)}`;
+      playerImage.classList.toggle("victory");
+      computerImage.classList.toggle("defeat");
+      currentGame.setAttribute("style", "color: #69b844");
+      if (gamesPlayed.length == 2) {
+        removeClass(previousRound, "hide");
+        removeClass(previousGame, "hide");
+      } else if (gamesPlayed.length > 2) {
+        removeClass(previousRoundTwo, "hide");
+        removeClass(previousGameTwo, "hide");
+      }
+    } else if (result === "loss") {
+      rounds.textContent = `Round: ${gamesPlayed.length}`;
+      losses.textContent = `Losses: ${scoreComputer.length}`;
+      quips.textContent = `${getComputerQuip(playerLoseQuips)}`;
+      playerImage.classList.toggle("defeat");
+      computerImage.classList.toggle("victory");
+      currentGame.setAttribute("style", "color: #FA8928ff");
+      if (gamesPlayed.length == 2) {
+        removeClass(previousRound, "hide");
+        removeClass(previousGame, "hide");
+      } else if (gamesPlayed.length > 2) {
+        removeClass(previousRoundTwo, "hide");
+        removeClass(previousGameTwo, "hide");
+      }
+    } else {
+      rounds.textContent = `Round: ${gamesPlayed.length}`;
+      ties.textContent = `Ties: ${tiedGames.length}`;
+      quips.textContent = `${getComputerQuip(playerTieQuips)}`;
+      currentGame.setAttribute("style", "color: black");
+      if (gamesPlayed.length == 2) {
+        removeClass(previousRound, "hide");
+        removeClass(previousGame, "hide");
+      } else if (gamesPlayed.length > 2) {
+        removeClass(previousRoundTwo, "hide");
+        removeClass(previousGameTwo, "hide");
+      }
+    }
+  });
+});
